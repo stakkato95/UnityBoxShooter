5d4b6dedbdc0a2c682e5fd7e69e6ccfa5acf962a
Artsiom Kaliaha <stakkato95@gmail.com>
2016-09-14 21:28:21 +0300

    added simple mover

---
+++ BoxShooter/Assets/Scripts/_Custom/BasicMover.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicMover : MonoBehaviour {
+
+	public float rotationPerSecond = 180;
+
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate (Vector3.up * rotationPerSecond * Time.deltaTime);
+	}
+}
