1435b31c8a75811e208a24d03bb3690411d4abd3
Artsiom Kaliaha <stakkato95@gmail.com>
2016-09-19 22:04:28 +0300

    added up-down motion

--- BoxShooter/Assets/Scripts/_Custom/BasicMover.cs
+++ BoxShooter/Assets/Scripts/_Custom/BasicMover.cs
@@ -4,9 +4,21 @@ using System.Collections;
 public class BasicMover : MonoBehaviour {
 
 	public float rotationPerSecond = 180;
+	public float motionUpAndDown = 0.1f;
+
+	public bool doSpin = true;
+	public bool doMotion = false;
 
 	// Update is called once per frame
 	void Update () {
-		gameObject.transform.Rotate (Vector3.up * rotationPerSecond * Time.deltaTime);
+		if (doSpin) {
+			//rotate arount the up axis
+			gameObject.transform.Rotate (Vector3.up * rotationPerSecond * Time.deltaTime);
+		}
+
+		if (doMotion) {
+			//move up and down
+			gameObject.transform.Translate (Vector3.down * Mathf.Cos (Time.timeSinceLevelLoad) * motionUpAndDown);
+		}
 	}
 }
