1e268d9ed064fe55db127f330bc707b572e718ec
Artsiom Kaliaha <stakkato95@gmail.com>
2016-09-23 16:31:46 +0300

    added base movement script

---
+++ BoxShooter/Assets/Scripts/_Custom/BasicController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicController : MonoBehaviour {
+
+	public float movementSpeed = 4;
+	public float gravity = 0.5f;
+
+	CharacterController CharController;
+
+	void Start() {
+		CharController = gameObject.GetComponent<CharacterController> ();	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var movementZ = Input.GetAxis ("Vertical") * Vector3.forward * movementSpeed * Time.deltaTime;
+
+		var movementX = Input.GetAxis ("Horizontal") * Vector3.right * movementSpeed * Time.deltaTime;
+
+		var movement = transform.TransformDirection (movementZ + movementX);
+
+		movement.y -= gravity * Time.deltaTime;
+
+		CharController.Move (movement);
+	}
+}
--- BoxShooter/Assets/Scripts/_Custom/BasicThreStateMover.cs
+++ BoxShooter/Assets/Scripts/_Custom/BasicThreStateMover.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class BasicThresStateMover : MonoBehaviour
+public class BasicThreStateMover : MonoBehaviour
 {
 
 	public enum Motion
