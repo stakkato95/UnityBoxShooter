2ba71d2c1a0ba0e6bccdb31e75c319f8889df8d4
Artsiom Kaliaha <stakkato95@gmail.com>
2016-10-02 18:45:02 +0300

    added basic platform mover; only movement forward in z direction works

---
+++ BoxShooter/Assets/PlatformMover.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformMover : MonoBehaviour
+{
+	public enum State
+	{
+		Moving,
+		Staying
+	}
+
+	public int secondsOfStay = 2;
+	float stayFinishTime;
+
+	State CurrentState = State.Staying;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.Log (Time.time);
+		if (stayFinishTime >= Time.time) {
+			CurrentState = State.Moving;
+		}
+
+		if ((transform.position.z <= 0 || 35 <= transform.position.z) && CurrentState == State.Moving) {
+			CurrentState = State.Staying;
+
+			stayFinishTime = Time.time + secondsOfStay;
+		}
+
+		if (CurrentState == State.Moving) {
+			transform.Translate (35 <= transform.position.z ? new Vector3 (0, 0, -0.05f) : new Vector3 (0, 0, 0.05f));
+		}
+	}
+}
