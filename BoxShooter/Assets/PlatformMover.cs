a9491e05ea1eb91a856d09e1bf05ef553fdcc6e1
Artsiom Kaliaha <stakkato95@gmail.com>
2016-10-02 21:00:04 +0300

    added basic script for moving a platform

--- BoxShooter/Assets/PlatformMover.cs
+++ BoxShooter/Assets/PlatformMover.cs
@@ -5,36 +5,60 @@ public class PlatformMover : MonoBehaviour
 {
 	public enum State
 	{
-		Moving,
-		Staying
+		MovingForth,
+		MovingBack,
+		StartMovingForth,
+		StartMovingBack,
+		StayingToMoveForth,
+		StayingToMoveBack
 	}
 
-	public int secondsOfStay = 2;
-	float stayFinishTime;
+	const int ResetedTimer = -1;
 
-	State CurrentState = State.Staying;
+	public int secondsOfStay = 2;
+
+	float stayFinishTime;
+	State CurrentState;
+	Vector3 Size;
 
 	// Use this for initialization
 	void Start ()
 	{
+		CurrentState = State.MovingForth;
+		stayFinishTime = ResetedTimer;
+		Size = GetComponent<BoxCollider> ().size;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		Debug.Log (Time.time);
-		if (stayFinishTime >= Time.time) {
-			CurrentState = State.Moving;
+		var stayTimeElapsed = stayFinishTime == ResetedTimer ? false : stayFinishTime < Time.time;
+
+		if (stayTimeElapsed) {
+			CurrentState = CurrentState == State.StayingToMoveBack ? State.StartMovingBack : State.StartMovingForth;
 		}
 
-		if ((transform.position.z <= 0 || 35 <= transform.position.z) && CurrentState == State.Moving) {
-			CurrentState = State.Staying;
+		if ((35 <= transform.position.z + Size.z && CurrentState == State.MovingForth) ||
+			(transform.position.z - Size.z <= 0 && CurrentState == State.MovingBack)) {
+			CurrentState = CurrentState == State.MovingForth ? State.StayingToMoveBack : State.StayingToMoveForth;
 
 			stayFinishTime = Time.time + secondsOfStay;
 		}
 
-		if (CurrentState == State.Moving) {
-			transform.Translate (35 <= transform.position.z ? new Vector3 (0, 0, -0.05f) : new Vector3 (0, 0, 0.05f));
+		Move ();
+	}
+
+	void Move ()
+	{
+		if (CurrentState == State.MovingForth || CurrentState == State.MovingBack ||
+		    CurrentState == State.StartMovingForth || CurrentState == State.StartMovingBack) {
+			transform.Translate (CurrentState == State.MovingBack || CurrentState == State.StartMovingBack ? new Vector3 (0, 0, -0.1f) : new Vector3 (0, 0, 0.1f));
+
+			if (CurrentState == State.StartMovingForth || CurrentState == State.StartMovingBack) {
+				CurrentState = CurrentState == State.StartMovingBack ? State.MovingBack : State.MovingForth;
+				stayFinishTime = ResetedTimer;
+			}
 		}
 	}
 }
