fca379760301f4f0e4af241a2d089a44c60dccaf
Artsiom Kaliaha <stakkato95@gmail.com>
2016-10-05 23:12:35 +0300

    added few methods to generalize platform movement script

--- BoxShooter/Assets/PlatformMover.cs
+++ BoxShooter/Assets/PlatformMover.cs
@@ -1,5 +1,6 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections.Generic;
+
+using UnityEngine;
 
 public class PlatformMover : MonoBehaviour
 {
@@ -13,34 +14,55 @@ public class PlatformMover : MonoBehaviour
 		StayingToMoveBack
 	}
 
+	public enum Axis { X, Y, Z }
+
 	const int ResetedTimer = -1;
 
 	public int secondsOfStay = 2;
+	public int platformSpeedMetersPerSecond = 1;
+	public Axis axisOfMoving = Axis.Z;
+	public int coordinateA = 35;
+	public int coordinateB = 0;
 
 	float stayFinishTime;
 	State CurrentState;
+
+	Vector3 MovementVector;
 	Vector3 Size;
+
+	int smallerCoordinate;
+	int biggerCoordinate;
+
+	Dictionary<Axis, Vector3> axisToDirections = new Dictionary<Axis, Vector3>()
+	{
+		{Axis.X, Vector3.right},
+		{Axis.Y, Vector3.up},
+		{Axis.Z, Vector3.forward},
+	};
 
 	// Use this for initialization
 	void Start ()
 	{
 		CurrentState = State.MovingForth;
 		stayFinishTime = ResetedTimer;
+
+		MovementVector = axisToDirections [axisOfMoving];
 		Size = GetComponent<BoxCollider> ().size;
+
+		smallerCoordinate = Mathf.Min (coordinateA, coordinateB);
+		biggerCoordinate = Mathf.Max (coordinateA, coordinateB);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		Debug.Log (Time.time);
 		var stayTimeElapsed = stayFinishTime == ResetedTimer ? false : stayFinishTime < Time.time;
 
 		if (stayTimeElapsed) {
 			CurrentState = CurrentState == State.StayingToMoveBack ? State.StartMovingBack : State.StartMovingForth;
 		}
 
-		if ((35 <= transform.position.z + Size.z && CurrentState == State.MovingForth) ||
-			(transform.position.z - Size.z <= 0 && CurrentState == State.MovingBack)) {
+		if (IsOutOfMovementArea()) {
 			CurrentState = CurrentState == State.MovingForth ? State.StayingToMoveBack : State.StayingToMoveForth;
 
 			stayFinishTime = Time.time + secondsOfStay;
@@ -53,12 +75,34 @@ public class PlatformMover : MonoBehaviour
 	{
 		if (CurrentState == State.MovingForth || CurrentState == State.MovingBack ||
 		    CurrentState == State.StartMovingForth || CurrentState == State.StartMovingBack) {
-			transform.Translate (CurrentState == State.MovingBack || CurrentState == State.StartMovingBack ? new Vector3 (0, 0, -0.1f) : new Vector3 (0, 0, 0.1f));
+			transform.Translate (CurrentState == State.MovingBack || CurrentState == State.StartMovingBack ? 
+				-MovementVector * Time.deltaTime * platformSpeedMetersPerSecond : 
+				MovementVector * Time.deltaTime * platformSpeedMetersPerSecond);
 
 			if (CurrentState == State.StartMovingForth || CurrentState == State.StartMovingBack) {
 				CurrentState = CurrentState == State.StartMovingBack ? State.MovingBack : State.MovingForth;
 				stayFinishTime = ResetedTimer;
 			}
 		}
+	}
+
+	float GetTargetAxisCurrentPosition()
+	{
+		return axisOfMoving == Axis.X ? 
+			transform.position.x : 
+			(axisOfMoving == Axis.Y ? 
+				transform.position.y : 
+				transform.position.z);
+	}
+
+	float GetTargetAxisCurrentSize()
+	{
+		return axisOfMoving == Axis.X ? Size.x : (axisOfMoving == Axis.Y ? Size.y : Size.z);
+	}
+
+	bool IsOutOfMovementArea()
+	{
+		return (coordinateA <= GetTargetAxisCurrentPosition () + GetTargetAxisCurrentSize () && CurrentState == State.MovingForth) ||
+		(GetTargetAxisCurrentPosition () - GetTargetAxisCurrentSize () <= coordinateB && CurrentState == State.MovingBack);
 	}
 }
