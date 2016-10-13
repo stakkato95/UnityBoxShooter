695ced7f0c98bbf392e946e7b2c41501e5150459
Artsiom Kaliaha <stakkato95@gmail.com>
2016-10-13 22:07:14 +0300

    crosshair aligned with camera

---
+++ BoxShooter/Assets/Scripts/_VR/VRRenderScale.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class VRRenderScale : MonoBehaviour {
+
+	public float renderScale = 1.25f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		VRSettings.renderScale = renderScale;
+	}
+}
